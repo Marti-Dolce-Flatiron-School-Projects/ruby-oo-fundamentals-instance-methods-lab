89d73f5f3640e1d5b13ebce70ca9e37be6414031
Martinique <me@martidolce.com>
2020-11-17 23:36:46 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+# Learning Goals
+# Define a class.
+# Build instance methods for the class.
+# Overview
+# In this lab, we will practice defining classes and building instance methods inside them. Specifically, you are going to be adding 2 instance methods to a Dog and Person class.
+#
+class Person
+	def talk
+		puts "Hello World!"
+	end
+	def walk
+		puts "The Person is walking"
+	end
+end
+
+jim = Person.new()
+jim.talk
+jim.walk
